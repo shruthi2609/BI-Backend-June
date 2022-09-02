4ab81dcbeec3fb136388a004b8c0a57c7243eedb
shruthi093 <jayasruthi.l@gmail.com>
2022-09-02 19:00:58 +0530

    http server - get method

---
+++ node js demo/index.js
@@ -0,0 +1,25 @@
+const http=require('http')
+const data={
+    username:'carry',
+    role:'admin'
+}
+const server=http.createServer((req,res)=>{
+    console.log(req)
+if(req.url==='/home'&&req.method==='GET'){
+    res.writeHead(200)
+    res.end("home page demo")
+}
+if(req.url==='/index'&&req.method==='GET'){
+    res.writeHead(200)
+    res.end("<h1>Index Page</h1>")
+}
+if(req.url==='/users'&&req.method==='GET'){
+    res.writeHead(200)
+    res.end(JSON.stringify(data))
+}
+else{
+    res.writeHead(404)
+    res.end("this resource is not found please check")
+}
+})
+server.listen(3001,()=>console.log('server started ...'))
